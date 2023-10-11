d2925bde3d54d6cba923940c757b8259153d0f7c
antoniojunio <juniordsj@gmail.com>
2023-10-11 08:27:39 -0300

    feat: buscando dados do mock

--- src/pages/cidades/ListagemDePessoas.tsx
+++ src/pages/cidades/ListagemDePessoas.tsx
@@ -1,27 +1,40 @@
+import { useEffect, useMemo, useState } from 'react'
+import { LinearProgress, Paper, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow } from '@mui/material'
 import { useSearchParams } from 'react-router-dom'
+
+import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService'
 import { FerramentasDaListagem } from '../../shared/components'
 import { LayoutBaseDePagina } from '../../shared/layouts'
-import { useEffect, useMemo } from 'react'
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService'
 import { useDebounce } from '../../shared/hooks'
+import { Environment } from '../../shared/environment'
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
-  const {debounce} = useDebounce(3000, false)
+  const {debounce} = useDebounce()
+
+  const [rows, setRows] = useState<IListagemPessoa[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [totalCount ,setTotalCount] = useState(0)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
   useEffect(() => {
+    setIsLoading(true)
 
     debounce(() => {
       PessoasService.getAll(1, busca)
         .then((result) => {
+          setIsLoading(false)
+
           if (result instanceof Error) {
             alert(result.message)
           } else {
             console.log(result)
+
+            setTotalCount(result.totalCount)
+            setRows(result.data)
           }
         })
     })
@@ -38,6 +51,41 @@ export const ListagemDePessoas: React.FC = () => {
           aoMudarTextoDeBusca={texto => setSearchParams({busca: texto}, {replace:true})}
         />
       }
-    />
+    >
+      <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ações</TableCell>
+              <TableCell>Nome completo</TableCell>
+              <TableCell>Email</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.id}>
+                <TableCell>Ações</TableCell>
+                <TableCell>{row.nomeCompleto}</TableCell>
+                <TableCell>{row.email}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+
+          {totalCount === 0 && !isLoading && (
+            <caption>{Environment.LISTAGEM_VAZIA}</caption>
+          )}
+
+          <TableFooter>
+            {isLoading && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant='indeterminate'/>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </LayoutBaseDePagina>
   )
 }
